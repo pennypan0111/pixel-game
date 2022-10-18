0e5a566cc7ce8d24cf27e1db885213da49417c8a
pennypan0111 <minipan0111@gmail.com>
2022-10-18 19:07:30 +0800

    1018-1907
    1. 新增子彈射擊
    2. 怪物生成

--- js/main.js
+++ js/main.js
@@ -1,17 +1,145 @@
-$(document).keydown(function (e) {
+// 定義玩家火箭
+const shooter = document.getElementById('player-box')
 
-    // console.log(e.keyCode);
+// 往上的函式
+function moveUp(){
+    let topPosition = window.getComputedStyle(shooter).getPropertyValue('top')
+    if(shooter.style.top === '0px'){
+        return
+    }
+    else{
+        let position = parseInt(topPosition)
+        position -= 4
+        shooter.style.top = `${position}px`
+    }
+}
 
-    if(e.keyCode == 40){ //向下鍵
-        $('.arrow').css('top','150px')
+// 往下的函式
+function moveDown(){
+    let topPosition = window.getComputedStyle(shooter).getPropertyValue('top')
+    if(shooter.style.top === '800px'){
+        return
+    }
+    else{
+        let position =parseInt(topPosition)
+        position += 4
+        shooter.style.top = `${position}px`
+    }
+}
 
-        let left = parseInt($('.rock').css('left'))
+// 發射飛彈
+function fireLaser(){
+    let laser = createLaserElement()
+    background.appendChild(laser)
+    moveLaser(laser)
+}
+
+// 設定子彈樣式及位置
+function createLaserElement(){
+    let xPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('left'))
+    let yPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('top'))
+    let newLaser = document.createElement('img')
+    newLaser.src = './img/bullet.png'
+    newLaser.classList.add('laser')
+    newLaser.style.left = `${xPosition}px`
+    newLaser.style.top = `${yPosition + 35}px`
+    return newLaser
+}
+
+// 子彈射出的移動及時間
+function moveLaser(laser){
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left)
+        let monsters = document.querySelectorAll('.monster')
+        monsters.forEach(monster => {
+            if(checkLaserCollision(laser, monster)){
+                monster.classList.remove('monster')
+                monster.classList.add('dead-monster')
+            }
+        })
+        if(xPosition === 1600){
+            laser.style.display = 'none'
+            laser.remove()
+        }
+        else{
+            laser.style.left = `${xPosition + 4}px`
+        }
+    }, 10)
+}
+
+
+// 火箭移動及發射的函式
+function letShipFly(event){
+    if(event.key === 'ArrowUp'){
+        event.preventDefault()
+        moveUp()
     }
-    else if(e.keyCode == 38){ //向上鍵
-        $('.arrow').css('top','30px')
+    else if(event.key === 'ArrowDown'){
+        event.preventDefault()
+        moveDown()
     }
-    else if(e.keyCode == 13){
-        $('.title , .players').css('display','none')
-        $('.rock').css('display','block')
+    else if(event.key === ' '){
+        event.preventDefault()
+        fireLaser()
     }
-});
+}
+window.addEventListener('keydown',letShipFly)
+
+
+
+// 設定怪物的樣式及位置
+const monsterImgs = ['./img/alien-01.png', './img/alien-02.png', './img/alien-03.png', './img/alien-04.png']
+
+function createMonster(){
+    let newMonster = document.createElement('img')
+    let monsterSpriteImg = monsterImgs[Math.floor(Math.random()*monsterImgs.length)]
+    newMonster.src = monsterSpriteImg
+    newMonster.classList.add('monster')
+    newMonster.style.left = '100%'
+    newMonster.style.top = `${Math.floor(Math.random() * 600 ) + 30}px`
+    background.appendChild(newMonster)
+    moveMonster(newMonster)
+}
+
+// 重複生成怪物
+window.setInterval(function(){
+    createMonster()
+}, 3000)
+
+
+// 怪物的移動及時間
+function moveMonster(monster){
+    let moveMonsterInterval = setInterval(() => {
+        let xPosition = parseInt(window.getComputedStyle(monster).getPropertyValue('left'))
+        if(xPosition <= 0){
+            monster.remove()
+        }
+        else{
+            monster.style.left = `${xPosition - 4}px`
+        }
+    },20)
+}
+
+
+// 
+function checkLaserCollision(laser, monster){
+    let laserLeft = parseInt(laser.style.left)
+    let laserTop = parseInt(laser.style.top)
+    let laserBottom = laserTop - 20
+
+    let monsterLeft = parseInt(monster.style.left)
+    let monsterTop = parseInt(monster.style.top)
+    let monsterBottom = monsterTop - 30
+
+    if(laserTop != 1600 && laserLeft + 40 >= monsterLeft){
+        if((laserTop <= monsterTop && laserTop >= monsterBottom)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    else{
+        return false
+    }
+}
