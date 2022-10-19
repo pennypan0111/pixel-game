5b75482b428f7bbad439858629e6e9ac9f0f7eca
pennypan0111 <minipan0111@gmail.com>
2022-10-19 18:52:30 +0800

    1019-1852

--- js/pixelGame-easy.js
+++ js/pixelGame-easy.js
@@ -33,7 +33,7 @@ function moveDown(){
 
 
 
-// 發射飛彈
+// 發射子彈
 function fireLaser(){
     let laser = createLaserElement()
     background.appendChild(laser)
@@ -60,17 +60,19 @@ function createLaserElement(){
 
 // 子彈射出的移動及時間
 const scoreCounter = document.querySelector('#score span')
+let killMonster = new Audio('./music/kill.mp3')
 
 function moveLaser(laser){
     let laserInterval = setInterval(() => {
         let xPosition = parseInt(laser.style.left)
         let monsters = document.querySelectorAll('.monster')
         monsters.forEach(monster => {
-            if(checkLaserCollision(laser, monster)){
-                monster.classList.remove('monster')
-                monster.classList.add('dead-monster')
-                laser.remove()
-                scoreCounter.innerText = parseInt(scoreCounter.innerText) + 10
+            if(checkLaserCollision(laser, monster)){ //子彈、怪物偵測碰撞時
+                monster.classList.remove('monster') //移除怪物
+                monster.classList.add('dead-monster') //怪物透明度變0，即消失
+                killMonster.play() //消滅怪物時的音效
+                laser.remove() //移除子彈
+                scoreCounter.innerText = parseInt(scoreCounter.innerText) + 10 //分數增加
             }
         })
         if(xPosition === 1600){
@@ -140,7 +142,7 @@ function moveMonster(monster){
             }
         })
         if(xPosition <= 0){
-            monster.remove()
+            monster.remove() //怪物到視窗最左邊時移除
         }
         else{
             monster.style.left = `${xPosition - 4}px`
@@ -185,10 +187,13 @@ function checkShooterCollision(shooter, monster){
 // 遊戲結束
 function gameOver(){
     window.removeEventListener('keydown', letShipFly)
+
     let monsters = document.querySelectorAll('.monster')
     monsters.forEach(monster => monster.remove())
+
     let lasers = document.querySelectorAll('.laser')
     lasers.forEach(laser => laser.remove())
+
     gameOverAlert() //遊戲結束提示框
     musicStart.pause() //背景音樂關閉
 
@@ -197,10 +202,10 @@ function gameOver(){
 
 
 // 遊戲結束提示框
-let gameOverMusic = new Audio('./music/game-over.mp3')
+let gameOverMusic = new Audio('./music/game-over.mp3') //設定玩家死亡的音效
 
 function gameOverAlert(){
-    gameOverMusic.play()
+    gameOverMusic.play() //啟動玩家死亡的音效
     Swal.fire({
         title: 'Game Over!',
         text: '你已被外星人綁架',
@@ -214,14 +219,14 @@ function gameOverAlert(){
             popup: 'animate__animated animate__fadeOutUp'
           }
     }).then((result) => {
-        window.location.reload();
-        gameOverMusic.pause();
+        window.location.reload(); //頁面刷新重整
+        gameOverMusic.pause(); //關閉玩家死亡的音效
     });
 }
 
 
 
-// 離開遊戲
+// 離開遊戲回到上一頁
 function esc(e){
     if(e.keyCode == 27){
         window.location.href = "pixelGame-start.html";
@@ -248,17 +253,17 @@ let bgrun = setInterval(function(){
 
 
 // 背景音樂開關設定
-let musicStart = new Audio('./music/bg-music.mp3')
+let musicStart = new Audio('./music/bg-music.mp3') //設定背景音效
 
-$('.music .fa-volume-xmark').click(function(s) {
+$('.music .fa-volume-xmark').click(function() {
     
     $('.fa-volume-xmark').css('display', 'none');
     $('.fa-volume-high').css('display', 'block');
-    musicStart.play();
+    musicStart.play(); //啟動背景音效
 })
 
-$('.music .fa-volume-high').click(function(s) {
+$('.music .fa-volume-high').click(function() {
     $('.fa-volume-high').css('display', 'none');
     $('.fa-volume-xmark').css('display', 'block');
-    musicStart.pause();
+    musicStart.pause(); //關閉背景音效
 })
